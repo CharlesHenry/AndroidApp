bdaa7934407ba0f05f47a33b2edcbf33a5b9dbc3
Charles Henry <charles.henry@live.com>
2013-03-05 01:48:06 +0000

    Implemented Splash screen and adjusted app name

--- src/com/ruzulive/androidapp/Splash.java
+++ src/com/ruzulive/androidapp/Splash.java
@@ -1,6 +1,7 @@
 package com.ruzulive.androidapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends Activity {
@@ -10,18 +11,21 @@ public class Splash extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash); //Sets content page to splash layout
+		
 		//Used as a timer for the Splash screen
 		Thread timer= new Thread() {
 			public void run(){
 				try{
-					
+					sleep(5000);
 				} catch(InterruptedException e){
-					
+					e.printStackTrace();
 				} finally {
-					
+					Intent mainAct = new Intent("com.ruzulive.androidapp.MAINACT"); //link to intent in AndroidManifest.xml
+					startActivity(mainAct);
 				}
-			};
+			}
 		};
+		timer.start();
 	}
 
 }
