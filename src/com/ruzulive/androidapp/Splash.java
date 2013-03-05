5523c03938bead79f5c3fc3c386e2c519599bbbe
Charles Henry <charles.henry@live.com>
2013-03-05 01:29:33 +0000

    Initial Commit

---
+++ src/com/ruzulive/androidapp/Splash.java
@@ -0,0 +1,27 @@
+package com.ruzulive.androidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		Thread timer= new Thread() {
+			public void run(){
+				try{
+					
+				} catch(InterruptedException e){
+					
+				} finally {
+					
+				}
+			};
+		};
+	}
+
+}
